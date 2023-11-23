19e0988108c014fc163cdae881457e7e7a386645
Ayush-004 <ayush2003malik@gmail.com>
2023-11-24 01:40:01 +0530

    add random_number_generater

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use num_bigint::BigUint;
+use num_bigint::{BigUint,RandBigInt};
 
 /// Performs modular exponentiation.
 ///
@@ -56,18 +56,23 @@ pub fn verify(r1: &BigUint,r2: &BigUint, y1: &BigUint,y2:&BigUint,alpha: &BigUin
    // If both conditions are true, the verification succeeds
    cond1 && cond2
 }
+
+pub fn generate_random_below(bound: &BigUint)-> BigUint{
+   let mut rng = rand::thread_rng();
+   rng.gen_biguint_below(bound)
+}
 #[cfg(test)]
 mod test{
    use super::*;
    #[test]
-   fn test_toy_example(){
+   fn test_toy_example_with_random_numbers(){
       let alpha = BigUint::from(4u32);
       let beta = BigUint::from(9u32);
       let p = BigUint::from(23u32);
       let q = BigUint::from(11u32);
       let x = BigUint::from(6u32);
-      let k = BigUint::from(7u32);
-      let c= BigUint::from(4u32);
+      let k =  generate_random_below(&q);
+      let c= generate_random_below(&q);
       let y1 = exponentiate(&alpha,&x,&p);
       let y2 = exponentiate(&beta,&x,&p);
       assert_eq!(y1,BigUint::from(2u32));
@@ -75,11 +80,8 @@ mod test{
 
       let r1 = exponentiate(&alpha,&k,&p);
       let r2 = exponentiate(&beta,&k,&p);
-      assert_eq!(r1,BigUint::from(8u32));
-      assert_eq!(r2,BigUint::from(4u32));
 
       let s = response(&k,&c,&x,&q);
-      assert_eq!(s,BigUint::from(5u32));
 
       let result = verify(&r1,&r2,&y1,&y2,&alpha,&beta,&c,&s,&p);
       assert!(result);
