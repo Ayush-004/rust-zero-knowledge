82cc5dfecc14d6f37a19d2b9e984ea8cb22d2222
Ayush-004 <ayush2003malik@gmail.com>
2023-11-24 19:43:05 +0530

    Implement gRPC Protobuf Definitions for ZKP Authentication

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main(){
+    tonic_build::configure()
+        .build_server(true)
+        .out_dir("src/")
+        .compile(
+            &["proto/zkp_auth.proto"],
+            &["proto/"],//specify the root location to search proto dependencies
+        ).unwrap();
+}
